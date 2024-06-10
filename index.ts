f876f87f1102ab5cce8bc468fd89453ad61fcf86
usuario <gabriel.alves@smartstaff.com.br>
2024-06-10 10:09:23 -0300

    Trying to fix port error on render

--- index.ts
+++ index.ts
@@ -8,12 +8,18 @@ const server = fastify()
 server.register(registerClientRoutes, { prefix: '/client' })
 server.register(registerDeveloperRoutes, { prefix: '/developer' })
 
-let port = process.env.PORT ?? 3000;
+let port = process.env.PORT || 3000;
 
 if (typeof port === 'string') {
-  port = parseInt(port);
+  port = parseInt(port, 10);
 }
 
-server.listen({ port })
+server.listen(port, '0.0.0.0', (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
 
 
