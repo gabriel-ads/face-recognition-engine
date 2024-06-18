6fdacba5180ad52eac67b9671da22babd7a45459
usuario <gabriel.alves@smartstaff.com.br>
2024-06-18 17:08:48 -0300

    refactoring bitbucket pipeline file

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#
-## Navegar até o diretório da aplicação
+
+# Navegue até o diretório do seu projeto
 cd face-recognition-engine/
-## Atualizar o repositório
-GIT_SSH_COMMAND="ssh -i /home/ubuntu/id_rsa -o StrictHostKeyChecking=no" git pull git@bitbucket.org/smartstaff/face-recognition-engine/src/main/
-#
+
 ## Instalar dependências clean install (ci)
 npm ci
-#
+
 ## Roda o build e cria uma pasta dist
 npm run build
-#
+
 ## Faz a migração de forma segura para o banco de Prod
 npx prisma db push
-#
+
 ## Reiniciar o serviço (Considerando que já esteja rodando)
 sudo systemctl restart recog.service
